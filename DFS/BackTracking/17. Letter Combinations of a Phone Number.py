4b000e13268db3afd492cdee00f5663dde7d1fb7
MTGTsunami <mtgtsunami1219@gmail.com>
2019-09-25 00:20:37 -0400

    09/25/2019

---
+++ DFS/BackTracking/17. Letter Combinations of a Phone Number.py
@@ -0,0 +1,50 @@
+"""
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+
+
+Example:
+
+Input: "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+Note:
+
+Although the above answer is in lexicographical order, your answer could be in any order you want.
+"""
+
+
+class MySolution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+
+        phone = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        def backtrack(idx, expr):
+            if idx == len(digits):
+                ans = "".join(expr)
+                out.append(ans)
+            else:
+                for ch in phone[digits[idx]]:
+                    expr.append(ch)
+                    backtrack(idx + 1, expr)
+                    expr.pop()
+
+        out = []
+        backtrack(0, [])
+        return out
