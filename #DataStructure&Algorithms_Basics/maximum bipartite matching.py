5ab8d9698d40627510bf02367e7e0f76fcef36b9
MTGTsunami <mtgtsunami1219@gmail.com>
2019-11-05 01:28:45 -0500

    11/05/2019

---
+++ #DataStructure&Algorithms_Basics/maximum bipartite matching.py
@@ -0,0 +1,27 @@
+def maxBPM(graph):
+    def bpm(u, seen):
+        for v in range(len(graph[0])):
+            if graph[u][v] == 1 and not seen[v]:
+                seen[v] = True
+                if match[v] == -1 or bpm(match[v], seen):
+                    match[v] = u
+                    return True
+        return False
+
+    match = [-1] * len(graph[0])
+    res = 0
+    for u in range(len(graph)):
+        seen = [False] * len(graph[0])
+        if bpm(u, seen):
+            res += 1
+    return res
+
+
+if __name__ == '__main__':
+    graph =[[0, 1, 1, 0, 0, 0],
+          [1, 0, 0, 1, 0, 0],
+          [0, 0, 1, 0, 0, 0],
+          [0, 0, 1, 1, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 1]]
+    print(maxBPM(graph))
